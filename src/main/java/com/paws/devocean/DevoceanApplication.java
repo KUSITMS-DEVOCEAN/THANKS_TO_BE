0315bbb40bf45349fe9523812faffea640136c4f
Keon <devkeon123@gmail.com>
2024-08-24 21:10:51 +0900

    Add files via upload

---
+++ src/main/java/com/paws/devocean/DevoceanApplication.java
@@ -0,0 +1,13 @@
+package com.paws.devocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoceanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoceanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paws/devocean/DevoceanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paws.devocean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoceanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
